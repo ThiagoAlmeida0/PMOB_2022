438a2794d0c3e0f539ae16e1205dbfb038fcae08
juliarodrigues10 <jvrm1@aluno.ifal.edu.br>
2022-09-05 00:03:18 +0000

    adicionado o ListView na tela

--- lib/ui/screens/math_matters.dart
+++ lib/ui/screens/math_matters.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pmob_22/ui/screens/question_screen.dart';
 import 'package:pmob_22/utils/constants.dart';
 
 class MathMatters extends StatefulWidget {
@@ -12,146 +11,114 @@ class MathMatters extends StatefulWidget {
 class _MathMattersState extends State<MathMatters> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
+    return Scaffold(
+      backgroundColor: Color(0xFFf7f7f2),
       appBar: AppBar(
         backgroundColor: mainColor,
-        title: Center(child: Text("Matemática")),
+        toolbarHeight: 80,
+        title: const Center(
+            child: Text("Matemática",
+                style: TextStyle(fontSize: 30, color: textColor))),
       ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(5),
-          child: Column(
-            children: [
-              GestureDetector(
-                onTap: (){            
-                  Navigator.of(context).pushNamed("/question");
-                },
-                child: ListTile(
-                  title: Text ('Potenciação',
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold
-                    ),
-                  ),
-                ),
+      body: ListView(
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed("/question");
+            },
+            child: const ListTile(
+              title: Text(
+                'Potenciação',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
               ),
-
-              Divider(
-                color: mainColor,
-                thickness: 2,
-                indent: 14,
-                endIndent: 14,
-              ),
-              ListTile(
-                title: Text ('Radiciação',
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-              Divider(
-                color: mainColor,
-                thickness: 2,
-                indent: 14,
-                endIndent: 14,
-              ),
-              ListTile(
-                title: Text ('MMC e MDC',
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-              Divider(
-                color: mainColor,
-                thickness: 2,
-                indent: 14,
-                endIndent: 14,
-              ),
-              ListTile(
-                title: Text ('Regra de três',
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold
-                  ),
-                ),
-                subtitle: Text ('Simples e composta',
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-              ),
-              Divider(
-                color: mainColor,
-                thickness: 2,
-                indent: 14,
-                endIndent: 14,
-              ),
-              ListTile(
-                title: Text ('Produtos notáveis',
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-              Divider(
-                color: mainColor,
-                thickness: 2,
-                indent: 14,
-                endIndent: 14,
-              ),
-              ListTile(
-                title: Text ('Função afim',
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-              Divider(
-                color: mainColor,
-                thickness: 2,
-                indent: 14,
-                endIndent: 14,
-              ),
-              ListTile(
-                title: Text ('Função quadrática',
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-              Divider(
-                color: mainColor,
-                thickness: 2,
-                indent: 14,
-                endIndent: 14,
-              ),
-            ],
+            ),
           ),
-        ),
+          const Divider(
+            color: mainColor,
+            thickness: 2,
+            indent: 14,
+            endIndent: 14,
+          ),
+          const ListTile(
+            title: Text(
+              'Radiciação',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Divider(
+            color: mainColor,
+            thickness: 2,
+            indent: 14,
+            endIndent: 14,
+          ),
+          const ListTile(
+            title: Text(
+              'MMC e MDC',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Divider(
+            color: mainColor,
+            thickness: 2,
+            indent: 14,
+            endIndent: 14,
+          ),
+          const ListTile(
+            title: Text(
+              'Regra de três',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+              'Simples e composta',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+          const Divider(
+            color: mainColor,
+            thickness: 2,
+            indent: 14,
+            endIndent: 14,
+          ),
+          const ListTile(
+            title: Text(
+              'Produtos notáveis',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Divider(
+            color: mainColor,
+            thickness: 2,
+            indent: 14,
+            endIndent: 14,
+          ),
+          const ListTile(
+            title: Text(
+              'Função afim',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Divider(
+            color: mainColor,
+            thickness: 2,
+            indent: 14,
+            endIndent: 14,
+          ),
+          const ListTile(
+            title: Text(
+              'Função quadrática',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Divider(
+            color: mainColor,
+            thickness: 2,
+            indent: 14,
+            endIndent: 14,
+          ),
+        ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: mainColor,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.book_outlined),
-              label: 'Matéria',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Perfil',
-            ),
-          ],
-          selectedItemColor: Colors.white),
     );
   }
 }
